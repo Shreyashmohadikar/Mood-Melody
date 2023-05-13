b225c07fceca5570e2acbdf18815a0b733434f8c
Shreyash <75601500+Shreyashmohadikar@users.noreply.github.com>
2023-05-13 17:49:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,13 @@ def songsSurprise():
     return render_template("songs_surprise.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+# if __name__ == "__main__":
+#     app.run(debug=False,host='0.0.0.0')
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
     
